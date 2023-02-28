d47a54e9ce20f94e45474656d41deb4b74985375
LucaFaedda <lucakarl91@gmail.com>
2023-02-28 17:12:05 +0100

    lanciato faker seeder

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Project;
+Use Illuminate\Support\Str;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i=0; $i<10; $i++){
+            $newproject = new Project;
+            $newproject->title = $faker->text(10);
+            $newproject->data_progetto = $faker->date('Y_m_d');
+            $newproject->difficoltà = $faker->randomDigit();
+            $newproject->descrizione = $faker->paragraph();
+
+            $newproject->save();
+
+        }
+    }
+}
