b2da10461b732cf86c4807f4c29ae9d92a9a9ceb
LucaFaedda <lucakarl91@gmail.com>
2023-03-01 15:56:35 +0100

    delete completato

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -104,6 +104,8 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
-        //
+        $project->delete();
+
+        return redirect()->route('admin.projects.index')->with('message', 'progetto eliminato correttamente');
     }
 }
